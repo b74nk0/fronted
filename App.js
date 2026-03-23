c5d430f32aa7022ad05b8aa1cec8631e0d069fe9
b74nk0 <angelacostajr@outlook.com>
2026-03-22 22:25:10 -0500

    Corrección vercel

--- App.js
+++ App.js
@@ -3,15 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
-import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Analytics>
           <StatusBar style="auto" />
-        </Analytics>
         <AppNavigator />
       </NavigationContainer>
     </AuthProvider>
