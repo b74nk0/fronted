18cd6f4e0c74c4f659bc076a9203dd29849bbb05
b74nk0 <angelacostajr@outlook.com>
2026-03-22 22:13:57 -0500

    Analytics

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <StatusBar style="auto" />
+        <Analytics>
+          <StatusBar style="auto" />
+        </Analytics>
         <AppNavigator />
       </NavigationContainer>
     </AuthProvider>
